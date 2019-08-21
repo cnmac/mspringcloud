2f746d2363ce18efb41607c643b5a79bd518ace4
marse <marsedely@163.com>
2019-08-21 22:23:38 +0800

    common response

---
+++ service/common/src/main/java/com/mooyle/cloud/common/advice/CommonResponseDataAdvice.java
@@ -0,0 +1,21 @@
+package com.mooyle.cloud.common.advice;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return null;
+    }
+}
